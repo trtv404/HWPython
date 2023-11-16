f65f90a5b34824a14f8f6bd059764fbd1aa206f6
trtv404 <tr_tatjana@mail.ru>
2023-11-16 17:30:58 +0700

    task 32 is done

---
+++ task32.py
@@ -0,0 +1,16 @@
+# Задача 32: Определить индексы элементов массива (списка), 
+# значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума
+# и не больше заданного максимума)
+
+import random
+length = int(input('Ведите длинну массива: '))
+list = [random.randint(0, 100) for _ in range(length)]
+print(list)
+
+min = int(input('Ведите минимум: '))
+max = int(input('Ведите максимум: '))
+
+i = 0
+while i < len(list):
+    if list[i] >= min and list[i] <= max: print(f'индекс {i}') 
+    i+=1
